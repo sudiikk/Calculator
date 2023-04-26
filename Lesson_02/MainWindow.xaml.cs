a2f9263b1ffa2bbddfaf9a593addc3622cf95062
Timofey Raykin <Timofey Raykin@DESKTOP-4QIU603>
2023-04-26 20:49:23 +0300

    Добавьте файлы проекта.

---
+++ Lesson_02/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+
+namespace Lesson_02
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            foreach (UIElement el in MainRoot.Children)
+            {
+                if(el is Button) {
+                    ((Button)el).Click += Button_Click;
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string str = (string)((Button)e.OriginalSource).Content;
+
+            if (str == "AC")
+                tb_1.Text = "";
+            else if (str == "=")
+            {
+                string value = new DataTable().Compute(tb_1.Text, null).ToString();
+                tb_1.Text = value;
+            }
+            else
+                tb_1.Text += str;
+        }
+    }
+}
